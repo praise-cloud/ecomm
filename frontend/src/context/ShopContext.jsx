d309fb26417dc63612265f31bd02e177195cc1a0
Praise-cloud <georgewillpraise1@example.com>
2024-12-28 19:10:09 +0100

    modified the ShopContext.jsx file for the addToCart function

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { products } from "../assets/frontend_assets/assets";
 
 export const ShopContext = createContext();
@@ -9,6 +9,31 @@ const ShopContextProvider = (props)=> {
     const delivery_fee = 10;
     const [search, setSearch] = useState("");
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItems,setCartItems] = useState({});
+
+
+    const addToCart = async (itemId, size)=> {
+        let cartData = structuredClone(cartItems);
+
+        if(cartData[itemId]){
+            if(cartData[itemId][size]){
+                cartData[itemId][size] += 1;
+            }
+            else {
+                cartData[itemId][size] = 1;
+            }
+        }
+        else{
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+
+        setCartItems(cartData)
+    }
+
+    useEffect(()=>{
+        console.log(cartItems);
+    },[cartItems])
 
     const value = {
         products,
@@ -17,7 +42,9 @@ const ShopContextProvider = (props)=> {
         search,
         setSearch,
         showSearch,
-        setShowSearch
+        setShowSearch,
+        cartItems,
+        addToCart
     }
 
     return (
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -3,11 +3,12 @@ import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { useEffect, useContext, useState } from 'react';
 import { assets } from '../assets/frontend_assets/assets';
+import RelatedProduct from '../components/RelatedProduct';
 
 const Product = () => {
 
     const { productId } = useParams();
-    const {products, currency } = useContext(ShopContext);
+    const {products, currency, addToCart } = useContext(ShopContext);
     const [productData, setProductData] = useState(false);
     const [image, setImage] = useState("");
     const [size, setSize] = useState("");
@@ -17,11 +18,9 @@ const Product = () => {
             if(item._id === productId){
                 setProductData(item)
                 setImage(item.image[0])
-                console.log(item);
                 return null;
             }
         })
-        
     }
 
     useEffect(() =>(
@@ -79,9 +78,30 @@ const Product = () => {
                             ))}
                         </div>
                     </div>
-                    <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-500">ADD TO CART</button>
+                    <button onClick={()=>addToCart(productData._id,size)} className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">ADD TO CART</button>
+                    <hr className="mt-8 sm:w-4/5"/>
+                    <div className="text-sm text-gray-500 mt-5 flex flex-col gap1">
+                        <p>100% Original product.</p>
+                        <p>Cash on delivery is available on this product.</p>
+                        <p>Easy return and exchange policy within 7 days</p>
+                    </div>
                 </div>    
         </div>
+
+        {/* Description and review Section */}
+        <div className="mt-8">
+            <div className="flex">
+                <b className="border px-5 py-3 text-sm">Description</b>
+                <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+            </div>
+            <div className="flex flex-col gap-4 border px-6 text-sm text-gray-500">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam officia quos nihil, itaque quidem repellat aperiam ullam velit? Unde in nesciunt distinctio suscipit aperiam hic illum laboriosam consectetur explicabo asperiores?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste eveniet deleniti ipsa doloribus accusantium dolor corrupti nemo excepturi similique reiciendis enim magnam obcaecati provident cumque fuga animi illum, quas fugiat?</p>
+            </div>
+        </div>
+                            {/* display related products */}
+            
+            <RelatedProduct category={productData.category} subCategory={productData.subCategory}/>
     </div>
   ) : <div className="opacity-0"></div>
 }
